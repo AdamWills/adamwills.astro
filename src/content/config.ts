cb0ee1d1994fb359e7fe806138f02b3fbe2e814a
Adam Wills <adam@adamwills.com>
2023-12-07 09:04:36 -0500

    first list done

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+
+// 2. Define a `type` and `schema` for each collection
+const listCollection = defineCollection({
+  type: "content", // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.coerce.string()),
+    updated: z.date(),
+  }),
+});
+
+export const collections = {
+  lists: listCollection,
+};
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        solid: "10px 10px 0px var(--tw-shadow-color)",
+      },
+      colors: {
+        "brand-red": {
+          50: "#fff1f1",
+          100: "#ffdfdf",
+          200: "#ffc5c4",
+          300: "#ff9c9b",
+          400: "#ff6361",
+          500: "#ff3330",
+          600: "#f11411",
+          700: "#d40d0a", // brand-red
+          800: "#a80e0c",
+          900: "#8a1412",
+          950: "#4c0403",
+        },
+        "brand-black": "#0d0d0a",
+        "brand-blue": "#101827",
+      },
+    },
+  },
+  plugins: [],
+};
